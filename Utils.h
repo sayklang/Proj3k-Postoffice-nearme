02d623c1ddf1a45725381b8d01594f54e0ed3907
Ratchasin <ratchasin.kar@student.mahidol.edu>
2025-04-16 16:02:52 +0000

    from branch batman

--- Utils.h
+++ Utils.h
@@ -1,16 +1,12 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include  <iostream>
+#include <iostream>
+#include <unistd.h> // สำหรับ usleep()
 
-// Function to clear the screen - marked as inline to avoid multiple definition errors
 inline void clearScreen() {
-    // ANSI escape codes to clear screen and move cursor to top-left
-    std::cout << "\033[2J\033[1;1H";
-    
-    // Alternative methods:
-    // system("clear");  // For Unix/Linux
-    // system("cls");    // For Windows
+    usleep(1000000); // หน่วงเวลา 1 วินาทีก่อนล้างหน้าจอ
+    std::cout << "\033[2J\033[1;1H"; // ล้างหน้าจอสำหรับ Linux
 }
 
 #endif // UTILS_H
